0446b27a330651cb939cef0c8559dec5db57a4bd
Asim Shamim <asim@asimshamim.com>
2023-02-21 02:40:09 -0500

    checkpoint, looks nicer

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -6,24 +6,18 @@ export default function BlogLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="h-full grid grid-cols-[0.5fr_3fr_0.5fr] grid-rows-[0.5fr_3fr_0.5fr]">
-      <header className="pt-6 pb-6 text-center lg:text-left col-start-2">
-        <h1 className="text-2xl text-cyan-200 font-extrabold underline">
-          <Link href="/">Asim's Idea Dump</Link>
+    <main>
+      <header className="pt-6 pb-6  lg:text-left col-start-2">
+        <h1 className="text-center lg:text-4xl text-2xl text-springfog font-extrabold underline">
+          Asim's Idea Dump
         </h1>
+        <h4 className="m-2 text-center">
+          I decided to make a place to put my thoughts out there
+        </h4>
       </header>
-      <section className="h-full row-start-2 col-start-2 text-lg md:text-2xl">
+      <section className="text-lightorange row-start-2 col-start-2 text-lg md:text-2xl">
         {children}
       </section>
-      <Footer />
-    </div>
-  )
-}
-
-function Footer() {
-  return (
-    <footer className="row-start-3 col-start-2 text-center">
-      <div className="flex text-xs h-full items-end">&copy; Asim 2022</div>
-    </footer>
+    </main>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,27 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="h-full grid grid-cols-1 place-items-center">
-      <header>
-        <h1 className="md:text-6xl text-4xl">
-          Hi, I'm{' '}
-          <span className="text-cyan-200" title="pronounced ah-sim">
-            Asim
-          </span>
-        </h1>
-      </header>
-      <section className="h-full">
-        <article className="md:text-2xl text-justify lowercase">
-          <p className="pb-4 animate-fade-in">i like all things web dev</p>
-          <p className="opacity-0 animate-fade-in animation-delay-[3s] [animation-fill-mode:forwards]">
-            <Link className="underline" href="/blog">
-              checkout my latest article
-            </Link>
-          </p>
-        </article>
-      </section>
-    </div>
+    <main>
+      <div className="flex flex-col items-center h-[calc(100%-3.5rem)] justify-evenly">
+        <header>
+          <h1 className="md:text-6xl text-4xl">
+            Hi, I'm{' '}
+            <span className="text-lightorange" title="pronounced ah-sim">
+              Asim
+            </span>
+          </h1>
+        </header>
+        <section>
+          <article className="md:text-2xl text-justify lowercase text-lightorange">
+            <p className="pb-4 animate-fade-in">i like all things web dev</p>
+            <p className="opacity-0 animate-fade-in animation-delay-[3s] [animation-fill-mode:forwards]">
+              <Link className="underline" href="/blog">
+                checkout my latest article
+              </Link>
+            </p>
+          </article>
+        </section>
+      </div>
+    </main>
   )
 }
